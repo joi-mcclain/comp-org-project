8e23c9017b64505ea71ef3fa9a66f9e272a797fa
joi-mcclain <joi-mcclain@users.noreply.github.com>
2018-10-16 15:48:09 -0400

    print the last number of the student ID and print a newline: add 2 to the previous number (3) and print the result, then add the system call code, then add 7 to 3 to get the ascii value for newline (10)

--- project.asm
+++ project.asm
@@ -1,19 +1,25 @@
 .text
 	main:
 		li $v0, 11
-		la $a0, 64   # @
+		la $a0, 64     # @
 		syscall
 		sub $v0, 10
-		sub $a0, 64  # 0
+		sub $a0, 64    # 0
 		syscall
-		addi $a0, 2  # 2
+		addi $a0, 2    # 2
 		syscall
-		addi $a0, 6  # 8
+		addi $a0, 6    # 8
 		syscall
-		sub $a0, 4   # 4
+		sub $a0, 4     # 4
 		syscall
-		sub $a0, 2   # 2
-		syscall      # 2
+		sub $a0, 2     # 2
+		syscall        # 2
 		syscall
-		sub $a0, 1   # 1
+		sub $a0, 1     # 1
+		syscall
+		syscall
+		addi $a0, 2    # 3
+		syscall
+		addi $v0, 10
+		addi $a0, 7    # [new line] (10)
 		syscall
