24136209b1c2410c4081b2004ef010dd1708a4d4
joi-mcclain <joi-mcclain@users.noreply.github.com>
2018-10-22 21:52:41 -0400

    add spacing and comments

--- project.asm
+++ project.asm
@@ -1,6 +1,8 @@
 .text
 	main:
 		li $v0, 11
+		
+		#student ID
 		la $a0, 64     # @
 		syscall
 		sub $v0, 10
@@ -20,8 +22,12 @@
 		addi $a0, 2    # 3
 		syscall
 		addi $v0, 10
+		
+		#newline
 		addi $a0, 7    # [newline] (10)
 		syscall
+		
+		# last name
 		addi $a0, 67   # M (77)
 		syscall
 		addi $a0, 22   # c (99)
@@ -35,16 +41,21 @@
 		addi $a0, 8    # i (105)
 		syscall
 		addi $a0, 5    # n (110)
+		
+		# characters between
 		syscall
 		sub $a0, 66    # , (44)
 		syscall
 		sub $a0, 12    # [space] (32)
 		syscall
+		
+		#first name
 		addi $a0, 42   # J (74)
 		syscall
 		addi $a0, 37   # o (111)
 		syscall
 		sub $a0, 6     # i (105)
 		syscall
+		
 		li $v0, 10
 		syscall
